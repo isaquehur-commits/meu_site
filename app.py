b862887676bc27dd0b341d1010fd967d96f1f996
Isaque Hur Franceschini Barbosa <isaque.barbosa@schindler.com>
2025-11-19 11:16:34 -0300

    Adicionando nova pagina

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Agora a rota principal (/) vai para sobre.html
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+# A segunda página será index.html
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
